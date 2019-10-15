615fca5feaaf335807b01610e5fb5cf08ec80c7d
Agustin Carrasco <asermax@gmail.com>
2019-10-15 00:11:54 -0300

    feature(button): add button component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './theme.css';
 
 export * from './components/SROnlyText';
+export * from './components/Button';
 export * from './components/Icons';
 export * from './components/Tabs';
 export * from './components/Calendar';
